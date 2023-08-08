3de8940787b22575ca8d0a5eb83b5296a842c9ae
Yuri <yurifc4@gmail.com>
2023-08-07 22:11:00 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import uvicorn, argparse, os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', required=True)
+
+args = parser.parse_args()
+
+os.environ['ENV'] = args.env
+
+ssl_keyfile = None
+ssl_certfile = None
+
+reload = True
+
+if args.env == 'production':
+    ssl_keyfile = ''
+    ssl_certfile = ''
+
+    reload = False
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app='server.main:app',
+        workers=4,
+        host='0.0.0.0',
+        port=9000,
+        ssl_certfile=ssl_certfile,
+        ssl_keyfile=ssl_keyfile,
+
+        reload=reload
+    )
